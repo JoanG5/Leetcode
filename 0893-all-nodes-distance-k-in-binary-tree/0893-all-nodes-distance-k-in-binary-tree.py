25a91a144c64abc8cb292fc57085c5b47a2748f1
Joan Guzman <107497667+JoanG5@users.noreply.github.com>
2024-07-23 21:33:03 -0400

    Time: 35 ms (80.09%), Space: 16.8 MB (48.18%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.py
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.py
@@ -7,41 +7,41 @@
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        if not root:
-            return []
-            
-        output = []
-        parents = {}
-        # Create a helper function to store parentNode in hashmap
-        def helper(curr):
-            if not curr:
-                return 
-            if curr.left:
-                parents[curr.left] = curr
-                helper(curr.left)
-            if curr.right:
-                parents[curr.right] = curr
-                helper(curr.right)
+        parent = {}
+        queue = deque()
+        queue.append(root)
 
-        helper(root)
-        seen = set()
-        # Create a second helper function to traverse from target node to parent and child nodes and find nodes k away.
+        # Create connection with parents for each node
+        while queue:
+            size = len(queue)
+            for _ in range(size):
+                node = queue.popleft()
+                if node.left:
+                    parent[node.left] = node
+                    queue.append(node.left)
+                if node.right:
+                    parent[node.right] = node
+                    queue.append(node.right)
 
-        def helper2(curr, dist):
-            if not curr or curr in seen or dist > k:
-                return
-            seen.add(curr)
-            if dist == k:
-                output.append(curr.val)
-                return 
-            if curr in parents:
-                helper2(parents[curr], dist + 1)
-            helper2(curr.left, dist + 1)
-            helper2(curr.right, dist + 1)
+        seen = {}
+        queue.append(target)
+        level = 0
 
-        helper2(target, 0)
-
-        return output
+        while queue:
+            if level == k:
+                return [node.val for node in queue]
+            size = len(queue)
+            for _ in range(size):
+                node = queue.popleft()
+                seen[node] = True
+                if node.left and not (node.left in seen):
+                    queue.append(node.left)
+                if node.right and not (node.right in seen):
+                    queue.append(node.right)
+                if node in parent and not (parent[node] in seen):
+                    queue.append(parent[node])
+            level += 1
+        return []
 
 
 
