f3af033bf7c645b4e1a3e5ad66435e82814688c7
Joan Guzman <107497667+JoanG5@users.noreply.github.com>
2024-08-06 19:41:53 -0400

    Time: 175 ms (36.61%), Space: 18 MB (66.46%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         hash = {}
+        res = nums[0]
         for num in nums:
             if num in hash: hash[num] += 1
             else: hash[num] = 1
+
+            if hash[num] > hash[res]:
+                res = num
         
-        return max(hash, key=lambda x: hash[x])
+        return res
