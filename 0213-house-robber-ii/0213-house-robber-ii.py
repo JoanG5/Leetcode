452c3c6168a2ef5044159f6745d7b10074833afd
Joan Guzman <107497667+JoanG5@users.noreply.github.com>
2024-09-27 15:10:32 -0400

    Time: 30 ms (90.55%), Space: 16.5 MB (34.24%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,22 +1,24 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-
-        if len(nums) <= 2:
-            return max(nums)
+        # s[0] = nums[0]
+        # s[1] = max(nums[0], nums[1])
+        # s[2] = max(nums[2] + s[0], s[1])
+        # s[n-1] = max(nums[n-1] + s[n-3], s[n-2])
+        # s[n] = max(nums[n] + (s[n - 2] - nums[0]), s[n - 1])
+        if len(nums) == 0: return 0
+        if len(nums) <= 2: return max(nums)
 
         def helper(start, end):
-            dp = [0] * len(nums)
-            dp[start] = nums[start]
-            dp[start + 1] = max(nums[start], nums[start + 1])
+            s = [0] * len(nums)
+            s[start] = nums[start]
+            s[start + 1] = max(nums[start], nums[start + 1])
 
             for i in range(start + 2, end):
-                dp[i] = max(nums[i] + dp[i - 2], dp[i - 1])
+                s[i] = max(nums[i] + s[i - 2], s[i - 1])
 
-            return dp[end - 1]
-
-        range1 = helper(0, len(nums) - 1)
+            return s[end - 1]
+        
+        range1 = helper(0, len(nums)-1)
         range2 = helper(1, len(nums))
 
         return max(range1, range2)
