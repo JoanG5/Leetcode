340f3ea087c7fdc46d1329b7f81b6b319992ac84
Joan Guzman <107497667+JoanG5@users.noreply.github.com>
2024-10-09 12:51:22 -0400

    Time: 561 ms (21.7%), Space: 31.1 MB (19.69%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -5,5 +5,4 @@ class Solution:
             if nums[i - 1] + nums[i] > nums[i]:
                 nums[i] += nums[i - 1]
             res = max(res, nums[i])
-        
-        return res
+        return res 
