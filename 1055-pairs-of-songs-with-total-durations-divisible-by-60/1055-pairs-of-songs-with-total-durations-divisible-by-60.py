d066a9988baeefd9e50232733934768e907a034e
Joan Guzman <107497667+JoanG5@users.noreply.github.com>
2024-10-23 16:16:21 -0400

    Time: 40 ms (93.34%), Space: 20.3 MB (94.33%) - LeetHub

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/1055-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/1055-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -4,6 +4,27 @@ class Solution:
         # Go through the list
         # If 60 - (cur % 60)   is NOT in the hash then add cur to it
         # If it is then add one to the res 
+        hash = {}
+        for num in time:
+            cur = num % 60
+            if cur in hash:
+                hash[cur] += 1
+            else:
+                hash[cur] = 1
+        res = 0
+        for num in time:
+            cur = num % 60
+            hash[cur] -= 1
+            find = (60 - cur) % 60
+            if find in hash:
+                res += hash[find]
+
+
+
+        return res
+
+
+        
         visited = {}
         res = 0
 
