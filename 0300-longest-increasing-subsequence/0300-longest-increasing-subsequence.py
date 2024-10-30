d9448599e0ad28c54777f8f05f027d3abb4886b0
Joan Guzman <107497667+JoanG5@users.noreply.github.com>
2024-10-30 15:54:16 -0400

    Time: 11 ms (82.79%), Space: 17 MB (18.12%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,13 +1,25 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [0] * len(nums)
-        dp[0] = 1
-        for i in range(1, len(nums)):
-            cur = 1
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    cur = max(cur, dp[j] + 1)
-            dp[i] += cur
-                    
-        return max(dp)
+        res = [nums[0]]
 
+        def binary_search(arr, target):
+            l, r = 0, len(arr)
+            
+            while l <= r:
+                mid = (l + r) // 2
+                if arr[mid] > target:
+                    r = mid - 1
+                elif arr[mid] < target:
+                    l = mid + 1
+                else:
+                    arr[mid] = target
+                    return
+            arr[l] = target
+
+        for i in range(1, len(nums)):
+            if res[-1] < nums[i]:
+                res.append(nums[i])
+            else:
+                binary_search(res, nums[i])
+		
+        return len(res)
