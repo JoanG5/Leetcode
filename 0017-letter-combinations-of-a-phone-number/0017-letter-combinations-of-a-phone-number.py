37be7ea9f706d1863497eb3e835eaffd9b0c7c00
Joan Guzman <107497667+JoanG5@users.noreply.github.com>
2024-11-06 14:26:39 -0500

    Time: 0 ms (100%), Space: 16.6 MB (29.11%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,17 +1,19 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        # Base case
+        # # If current i >= len(digits)
         if len(digits) <= 0:
             return []
         res = []
         phone = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7":"pqrs", "8":"tuv", "9": "wxyz"}
 
         def helper(i, s):
-            if i == len(digits):
+            if i >= len(digits):
                 res.append(s[:])
                 return
-            for letter in phone[digits[i]]:
-                helper(i + 1, s + letter)
-
+            for j in phone[digits[i]]:
+                helper(i + 1, s + j)
+        
         helper(0, "")
 
-        return res 
+        return res
