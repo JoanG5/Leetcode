0ef3876f2499a17976654776de774d86ec04a200
Joan Guzman <107497667+JoanG5@users.noreply.github.com>
2024-11-13 12:27:35 -0500

    Time: 403 ms (46.24%), Space: 16.8 MB (80.83%) - LeetHub

--- 3209-minimum-number-of-coins-for-fruits/3209-minimum-number-of-coins-for-fruits.py
+++ 3209-minimum-number-of-coins-for-fruits/3209-minimum-number-of-coins-for-fruits.py
@@ -1,15 +1,13 @@
 class Solution:
     def minimumCoins(self, prices: List[int]) -> int:
         '''
-        Min number of coins on index i
-        s[1] = p[1]
-        s[2] = s[1] + p[2], s[1] + p[3], s[1] + p[4] 
-        s[3] = s[2] + p[3], s[2] + p[4], s[2] + p[5], s[2] + p[6] 
+        s = [0, 3, 4, ]
         '''
         n = len(prices)
         s = [0] + [float('inf')] * n
-        for i in range(1, n + 1):
+
+        for i in range(1, len(prices) + 1):
             for j in range(i, min(i + i, n) + 1):
-                s[j] = min(s[j], s[i - 1] + prices[i - 1])
+                s[j] = min(s[j], prices[i - 1] + s[i - 1])
         
-        return s[-1]
+        return s[n]
