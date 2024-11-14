3cd9aa78802ce19c3b53de142b9fda3cfce3e7e6
Joan Guzman <107497667+JoanG5@users.noreply.github.com>
2024-11-13 21:11:23 -0500

    Time: 21 ms (93.52%), Space: 17.9 MB (90.66%) - LeetHub

--- 1738-maximal-network-rank/1738-maximal-network-rank.py
+++ 1738-maximal-network-rank/1738-maximal-network-rank.py
@@ -1,19 +1,23 @@
 class Solution:
     def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
-        adj = defaultdict(list)
-        for start, target in roads:
-            adj[start].append(target)
-            adj[target].append(start)
-
+        degree = [0] * n
+        connected = [[False] * n for _ in range(n)]
         res = 0
-
+        for road in roads:
+            cur, target = road
+            degree[cur] += 1
+            degree[target] += 1
+            connected[cur][target] = True
+            connected[target][cur] = True
+        
         for i in range(n):
             for j in range(i + 1, n):
-                diff = 0
-                for neigh in adj[j]:
-                    if neigh == i:
-                        diff += 1
-                res = max(res, len(adj[i]) + len(adj[j]) - diff)
+                cur_rank = degree[i] + degree[j]
+
+                if connected[i][j]:
+                    cur_rank -= 1
+            
+                res = max(res, cur_rank)
         
         return res
                 
