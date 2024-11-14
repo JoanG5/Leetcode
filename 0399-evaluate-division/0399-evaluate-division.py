1e0df21d339481f4305bde6574cbe0166b342b44
Joan Guzman <107497667+JoanG5@users.noreply.github.com>
2024-11-14 12:30:48 -0500

    Time: 0 ms (100%), Space: 16.8 MB (45.89%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,30 +1,34 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         adj = defaultdict(list)
-        hashmap = defaultdict(dict)
-        res = []
-        for (x, y), value in zip(equations, values):
-            adj[x].append(y)
-            adj[y].append(x)
-            hashmap[x][y] = value
-            hashmap[y][x] = 1.0 / value
+        for i in range(len(equations)):
+            start, end = equations[i][0], equations[i][1]
+            adj[start].append((end, values[i]))
+            adj[end].append((start, 1.0/values[i]))
 
-        def dfs(i, j, cur, visited):
-            if not i in hashmap or j not in hashmap:
+        def dfs(i, end, val):
+            if i not in adj or end not in adj:
                 return -1.0
-            if i == j:
-                return cur
+            
+            if i == end:
+                return val
+
+            if i in visited:
+                return -1.0
 
             visited.add(i)
-            for neigh in adj[i]:
-                if not neigh in visited:
-                    result = dfs(neigh, j, cur * hashmap[i][neigh], visited)
+            for neighbor, div in adj[i]:
+                if not neighbor in visited:
+                    result = dfs(neighbor, end, val * div)
                     if result != -1.0:
-                        return result
+                        return result      
+
             visited.remove(i)
             return -1.0
 
-        for s, f in queries: 
-            res.append(dfs(s, f, 1.0, set()))
+        res = []
+        for x, y in queries:
+            visited = set()
+            res.append(dfs(x, y, 1.0))
         
         return res
