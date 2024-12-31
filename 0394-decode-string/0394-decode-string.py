e489d4d9ecacdacbfc538bf7a39d27daf406f806
Joan Guzman <107497667+JoanG5@users.noreply.github.com>
2024-12-31 15:05:39 -0500

    Time: 0 ms (100%), Space: 12.4 MB (9.51%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,21 +1,19 @@
 class Solution(object):
     def decodeString(self, s):
-        temp = ""
         stack = []
+        t = ""
         cur = 0
+
         for c in s:
             if c.isdigit():
                 cur = cur * 10 + int(c)
             elif c == "[":
-                stack.append((cur, temp))
-                cur = 0
-                temp = ''
+                stack.append((cur, t))
+                t = ''
+                cur = 0    
             elif c == "]":
-                num, newS = stack.pop()
-                temp = newS + num * temp
+                num, nt = stack.pop()
+                t = nt + num * t
             else:
-                temp += c
-        
-        return temp
-
-        
+                t += c
+        return t
