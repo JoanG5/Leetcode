186ec389ba6cb3fb33ad7580755f2ff1c4e316cb
Joan Guzman <107497667+JoanG5@users.noreply.github.com>
2025-01-01 14:50:50 -0500

    Time: 16 ms (9.05%), Space: 16.8 MB (6.78%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
@@ -2,10 +2,10 @@ class Solution(object):
     def allPathsSourceTarget(self, graph):
         res = []
         n = len(graph)
-        cycle = set()
+        # cycle = set()
         def dfs(i, cur):
-            if i in cycle:
-                return
+            # if i in cycle:
+            #     return
 
             if i == n - 1:
                 cur.append(i)
@@ -13,13 +13,13 @@ class Solution(object):
                 return
 
             cur.append(i)
-            cycle.add(i)
+            # cycle.add(i)
 
             for neigh in graph[i]:
                 dfs(neigh, cur[:])
             
-            cycle.remove(i)
-            
+            # cycle.remove(i)
+
         dfs(0, [])
         return res
         
