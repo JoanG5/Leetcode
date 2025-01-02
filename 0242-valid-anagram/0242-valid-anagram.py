7a37a85cc1feddeb18c7ad703b196f13792b9c39
Joan Guzman <107497667+JoanG5@users.noreply.github.com>
2025-01-02 14:19:28 -0500

    Time: 26 ms (26.36%), Space: 12.4 MB (65.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,13 +1,9 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t): return False
-
-        counter1 = {}
-        counter2 = {}
-        for i in range(len(s)):
-            if s[i] in counter1: counter1[s[i]] += 1
-            else: counter1[s[i]] = 1
-            if t[i] in counter2: counter2[t[i]] += 1
-            else: counter2[t[i]] = 1
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
         
-        return counter1 == counter2
+        count_s, count_t = Counter(s), Counter(t)
+
+        return count_s == count_t
+        
