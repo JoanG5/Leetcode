3458c921e5c4aeaaa1c2ff60a791969192cf6f95
Joan Guzman <107497667+JoanG5@users.noreply.github.com>
2025-01-02 17:51:37 -0500

    Time: 283 ms (44.15%), Space: 55.8 MB (59.01%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,35 +1,34 @@
 class RandomizedSet(object):
 
     def __init__(self):
-        self.numList = []
-        self.numMap = {}
+        self.map = {}
+        self.list = []
 
     def insert(self, val):
-        res = val not in self.numMap
+        res = val not in self.map
         if res:
-            self.numMap[val] = len(self.numList)
-            self.numList.append(val)
-        
-        return res 
+            self.map[val] = len(self.list)
+            self.list.append(val)
+        return res
         
 
     def remove(self, val):
-        res = val in self.numMap
+        res = val in self.map
         if res:
-            i = self.numMap[val]
-            lastVal = self.numList[-1]
-            self.numList[i] = lastVal
-            self.numList.pop()
-            self.numMap[lastVal] = i 
-            del self.numMap[val]
+            i = self.map[val]
+            lastVal = self.list[-1]
+            self.list[i] = lastVal
+            self.list.pop()
+            self.map[lastVal] = i 
+            del self.map[val]
         return res
-
-    def getRandom(self):
-        size = len(self.numList)
-        i = random.randint(0, size - 1)
-        return self.numList[i]
         
 
+    def getRandom(self):
+        size = len(self.list)
+        i = random.randint(0, size - 1)
+        return self.list[i]
+        
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
