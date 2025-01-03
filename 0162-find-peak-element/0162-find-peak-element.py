db2484e97e38070a86eec21bc009cf9dc51e8662
Joan Guzman <107497667+JoanG5@users.noreply.github.com>
2025-01-03 10:46:35 -0500

    Time: 0 ms (100%), Space: 12.6 MB (11.56%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,15 +1,11 @@
 class Solution(object):
     def findPeakElement(self, nums):
         l, r = 0, len(nums) - 1
-        mid = 0
         while l < r:
             mid = (l + r) // 2
-
-            if nums[mid - 1] < nums[mid] > nums[mid + 1]:
-                return mid
-            elif nums[mid + 1] > nums[mid]:
+            if nums[mid + 1] > nums[mid]:
                 l = mid + 1
             else:
-                r = mid - 1
+                r = mid
         
-        return mid
+        return l
