c73407b0d8e72b84bba28c446c6c0222909450f5
Joan Guzman <107497667+JoanG5@users.noreply.github.com>
2025-01-26 10:51:51 -0500

    Time: 2 ms (77.87%), Space: 18.1 MB (9.89%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,22 +5,61 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        tempNode = cur = ListNode()
-        remainder = 0
-    
-        while l1 or l2 or remainder:
-            num1 = l1.val if l1 else 0
-            num2 = l2.val if l2 else 0
+        # def reverse(head):
+        #     prev = None
+        #     while head:
+        #         temp = head.next
+        #         head.next = prev
+        #         prev = head
+        #         head = temp
+        #     return prev
         
-            tot = num1 + num2 + remainder
-            if tot >= 10: remainder = 1
-            else: remainder = 0
+        # l1 = reverse(l1)
+        # l2 = reverse(l2)
 
-            cur.next = ListNode(tot % 10)
-            cur = cur.next
-
-            if l1: l1 = l1.next
-            if l2: l2 = l2.next
+        temp = ListNode()
+        res = temp
+        carry = 0
+        while l1 and l2:
+            curVal = l1.val + l2.val + carry
+            if curVal >= 10:
+                carry = 1
+                curVal = curVal % 10
+            else:
+                carry = 0
+            temp.next = ListNode(curVal)
+            temp = temp.next
+            l1 = l1.next
+            l2 = l2.next
         
-        return tempNode.next
+        while l1:
+            curVal = carry + l1.val
+            carry = 0
+            if curVal >= 10:
+                carry = 1
+                curVal = curVal % 10
+            else:
+                carry = 0
+            l1 = l1.next
+            temp.next = ListNode(curVal)
+            temp = temp.next
+        while l2:
+            curVal = carry + l2.val
+            carry = 0
+            if curVal >= 10:
+                carry = 1
+                curVal = curVal % 10
+            else:
+                carry = 0
+            l2 = l2.next
+            temp.next = ListNode(curVal)
+            temp = temp.next
+        
+        if carry == 1:
+            temp.next = ListNode(carry)
+            temp = temp.next
+        
+        return res.next
+        
+        
 
