f1b3e0bac8207dd32a6bf8c9c43d441a191b07f8
Joan Guzman <107497667+JoanG5@users.noreply.github.com>
2025-01-29 09:08:34 -0500

    Time: 56 ms (58.33%), Space: 23.1 MB (7.09%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.py
+++ 0436-find-right-interval/0436-find-right-interval.py
@@ -1,26 +1,25 @@
 class Solution:
     def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
         n = len(intervals)
-
         for i in range(n):
             intervals[i] = (intervals[i], i)
         intervals.sort()
-
         res = [-1] * n
 
-        for i in range(n):
-            start, end, index = intervals[i][0][0], intervals[i][0][1], intervals[i][1]
+        for interval, i in intervals:
+            start, end = interval
             left, right = 0, n - 1
 
             while left <= right:
                 mid = (left + right) // 2
-                nStart = intervals[mid][0][0]
-                if nStart < end:
+
+                if intervals[mid][0][0] < end:
                     left = mid + 1
                 else:
                     right = mid - 1
- 
+            
             if left < n and intervals[left][0][0] >= end:
-                res[index] = intervals[left][1]
+                res[i] = intervals[left][1]
         
         return res
+        
