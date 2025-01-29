4a426160f6df1a0dd88dc402d9e86ead8e28d2a4
Joan Guzman <107497667+JoanG5@users.noreply.github.com>
2025-01-29 09:46:14 -0500

    Time: 4440 ms (50.19%), Space: 17.8 MB (63.5%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,21 +1,23 @@
-class Solution(object):
-    def exist(self, board, word):
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
         rows, cols = len(board), len(board[0])
-        seen = set()
-        def dfs(r, c, i):
-            if 0 <= r < rows and 0 <= c < cols and (r, c) not in seen and board[r][c] == word[i]:
-                if i + 1 == len(word):
-                    return True
-                seen.add((r, c))
-                res = dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1)
-                seen.remove((r,c))
-                return res
-            return False
+        visited = set()
         
-        for r in range(rows):
-            for c in range(cols):
-                if dfs(r, c, 0):
+        def dfs(r, c, i):
+            if i >= len(word):
+                return True
+            
+            if 0 <= r < rows and 0 <= c < cols and board[r][c] == word[i] and (r, c) not in visited:
+                visited.add((r, c))
+                value = dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1)
+                visited.remove((r, c))
+                return value
+            
+            return False
+
+        for row in range(rows):
+            for col in range(cols):
+                if dfs(row, col, 0):
                     return True
         
         return False
-        
